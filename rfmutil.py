6644460d8b9e8ac6533b5f8a481fd91dfb1056a6
liyou <wimxpliyou@gmail.com>
2015-01-19 20:33:00 +0800

    Update rfmutil.py 	1.change the file's openmode to binary 	2.remove the unicode for write file

--- rfmutil.py
+++ rfmutil.py
@@ -4,18 +4,18 @@ import sys
 
 buffersize=1000	
 def GetFileFromSocket(in_socket,recv_filename):
-	fdfile=io.open(recv_filename,'a')
+	fdfile=io.open(recv_filename,'ab')
 	while(1):
 		filebuffer=in_socket.recv(buffersize)
 		if(len(filebuffer)==0):
 			break;
 		print("get's  ",filebuffer)
-		fdfile.write(unicode(filebuffer))
+		fdfile.write(filebuffer)
 	fdfile.flush()
 	fdfile.close()
 
 def SendFileToSocket(in_socket,send_filename):
-	fdfile=io.open(send_filename,'r')
+	fdfile=io.open(send_filename,'rb')
 	while(1):
 		filebuffer=fdfile.read(buffersize)
 		if(len(filebuffer)==0):
