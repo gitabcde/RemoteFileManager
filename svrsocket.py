140c144de028431efd2a068334012ead46bc1907
liyou <wimxpliyou@gmail.com>
2015-01-21 00:05:59 +0800

    svrsocket.py 	Add multi-thread support 		Now server will create a thread for every client,so server can be operated by multi-user

--- svrsocket.py
+++ svrsocket.py
@@ -3,24 +3,34 @@
 import socket
 import rfmutil
 import sys
+import thread
+
+def svrthreadfun(retsocket,kargs):
+	print(kargs)
+	while(True):
+		cmdstr=rfmutil.WaitCommand(retsocket)
+		if(cmdstr.endswith('quit',0,4)):
+			break
+		rfmutil.DebugOutput(cmdstr)
+		if(cmdstr.endswith('get',0,3)):
+			rfmutil.DebugOutput('command type:get')
+			rfmutil.SendResponeCommand(retsocket)
+			rfmutil.DebugOutput('send get responecommand')
+			rfmutil.SendFileToSocket(retsocket,cmdstr.split(' ')[1])
+		if(cmdstr.endswith('put',0,3)):
+			rfmutil.DebugOutput('command type:put')
+			rfmutil.SendResponeCommand(retsocket)
+			rfmutil.DebugOutput('send put responecommand')
+			rfmutil.GetFileFromSocket(retsocket,cmdstr.split(' ')[1]+'get')
+	retsocket[0].close()
+	thread.exit_thread()
+
+
+
 svrsocket=socket._socketobject()
 svraddr=(sys.argv[1],int(sys.argv[2]))
 svrsocket.bind(svraddr)
 svrsocket.listen(5)
 while(True):
 	retsocket=svrsocket.accept()
-	cmdstr=rfmutil.WaitCommand(retsocket[0])
-	rfmutil.DebugOutput(cmdstr)
-	if(cmdstr.endswith('get',0,3)):
-		rfmutil.DebugOutput('command type:get')
-		rfmutil.SendResponeCommand(retsocket[0])
-		rfmutil.DebugOutput('send get responecommand')
-		rfmutil.SendFileToSocket(retsocket[0],cmdstr.split(' ')[1])
-	if(cmdstr.endswith('put',0,3)):
-		rfmutil.DebugOutput('command type:put')
-		rfmutil.SendResponeCommand(retsocket[0])
-		rfmutil.DebugOutput('send put responecommand')
-		rfmutil.GetFileFromSocket(retsocket[0],cmdstr.split(' ')[1]+'get')
-	retsocket[0].close()
-		
-
+	thread.start_new_thread(svrthreadfun,(retsocket[0],'kargs'))
